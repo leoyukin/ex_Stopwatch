f643d839dcdd224ae8b3fe340e9c3a90a117e5c2
Anakin Beckles <anakinyuku@gmail.com>
2026-08-10 13:10:06 -0400

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/ex_Stopwatch/',  // ← your repo name, exactly
+    base: '/ex_Stopwatch/',
 })
 
